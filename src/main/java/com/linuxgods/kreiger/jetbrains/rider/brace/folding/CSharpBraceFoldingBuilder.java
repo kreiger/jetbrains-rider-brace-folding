1f281fb96f96ee9bd87cbb84e5dd8c1f5bed4d00
Christoffer Hammarström <christoffer.hammarstrom@gmail.com>
2025-02-27 18:45:41 +0100

    Fold empty lines

--- src/main/java/com/linuxgods/kreiger/jetbrains/rider/brace/folding/CSharpBraceFoldingBuilder.java
+++ src/main/java/com/linuxgods/kreiger/jetbrains/rider/brace/folding/CSharpBraceFoldingBuilder.java
@@ -7,8 +7,12 @@ import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.FoldingGroup;
 import com.intellij.openapi.util.TextRange;
+import com.intellij.patterns.ElementPattern;
+import com.intellij.patterns.PlatformPatterns;
+import com.intellij.patterns.StandardPatterns;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiRecursiveElementVisitor;
+import com.intellij.psi.PsiWhiteSpace;
 import com.intellij.psi.tree.IElementType;
 import com.jetbrains.rider.languages.fileTypes.csharp.psi.impl.CSharpParameterDeclaration;
 import org.jetbrains.annotations.NotNull;
@@ -25,9 +29,29 @@ public class CSharpBraceFoldingBuilder extends FoldingBuilderEx {
 
     @Override
     public FoldingDescriptor @NotNull [] buildFoldRegions(@NotNull PsiElement psiElement, @NotNull Document document, boolean b) {
-        FoldingGroup foldingGroup = FoldingGroup.newGroup("brace");
+        FoldingGroup braceGroup = FoldingGroup.newGroup("brace");
+        FoldingGroup spaceGroup = FoldingGroup.newGroup("space");
         List<FoldingDescriptor> descriptors = new ArrayList<>();
         psiElement.accept(new PsiRecursiveElementVisitor() {
+            @Override
+            public void visitWhiteSpace(@NotNull PsiWhiteSpace space) {
+                ASTNode node = space.getNode();
+                ASTNode prev = node.getTreePrev();
+                if (prev != null && prev.getElementType() == WHITE_SPACE) return;
+                ASTNode next = node.getTreeNext();
+                if (next == null || next.getElementType() != WHITE_SPACE) return;
+                ASTNode nextNext = next.getTreeNext();
+                if (nextNext == null || nextNext.getElementType() != WHITE_SPACE) return;
+                TextRange textRange = next.getTextRange();
+                for (ASTNode n = nextNext.getTreeNext(); n != null; n = n.getTreeNext()) {
+                    if (n.getElementType() != WHITE_SPACE) {
+                        FoldingDescriptor foldingDescriptor = new FoldingDescriptor(next, textRange, spaceGroup, "");
+                        descriptors.add(foldingDescriptor);
+                        return;
+                    }
+                    textRange = textRange.union(n.getTreePrev().getTextRange());
+                }
+            }
 
             @Override
             public void visitElement(@NotNull PsiElement element) {
@@ -56,7 +80,7 @@ public class CSharpBraceFoldingBuilder extends FoldingBuilderEx {
                 } else if (textRange.getLength() == 1 && treePrev.getText().charAt(0) != '\n') {
                     return;
                 }
-                FoldingDescriptor foldingDescriptor = new FoldingDescriptor(astNode, textRange, foldingGroup);
+                FoldingDescriptor foldingDescriptor = new FoldingDescriptor(astNode, textRange, braceGroup, " ");
                 descriptors.add(foldingDescriptor);
             }
         });
@@ -66,7 +90,7 @@ public class CSharpBraceFoldingBuilder extends FoldingBuilderEx {
 
     @Override
     public @Nullable String getPlaceholderText(@NotNull ASTNode astNode) {
-        return " ";
+        return null;
     }
 
     @Override
